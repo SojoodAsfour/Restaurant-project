30052c39b1e4676983448b3619b123b05f8cee1f
SojoodAsfour <sojoudnazih@gmail.com>
2026-08-03 21:50:21 +0300

    add dark mode - about page

---
+++ assets/js/about.js
@@ -0,0 +1,24 @@
+const themeToggle = document.querySelector("#theme-toggle");
+const rootElement = document.documentElement;
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark" || !savedTheme) {
+    rootElement.classList.add("dark");
+} else {
+    rootElement.classList.remove("dark");
+}
+
+function updateThemeButtonLabel() {
+    const isDarkMode = rootElement.classList.contains("dark");
+
+}
+
+if (themeToggle) {
+    updateThemeButtonLabel();
+    themeToggle.addEventListener("click", () => {
+        const isDarkMode = rootElement.classList.toggle("dark");
+        localStorage.setItem("theme",isDarkMode ? "dark" : "light");
+        updateThemeButtonLabel();
+    });
+}
